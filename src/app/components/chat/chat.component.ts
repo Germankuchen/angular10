fcc59ce772a93c6ab2ef889c5a8e716cf3f5eb0c
germankuchen <germankuchen@gmail.com>
2019-04-30 08:58:35 -0300

    Cambios

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../providers/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -8,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class ChatComponent implements OnInit {
 
   mensaje: string;
+  info: any[];
 
   enviarMensaje() {
     console.log('Agrengando ' + this.mensaje);
   }
 
-  constructor() { }
+  constructor(private _chatService: ChatService) {
+    console.log('Aca invocamos el servicio');
+    this.info = this._chatService.cargarMensajes();
+  }
 
   ngOnInit() {
   }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  private itemsCollection: AngularFirestoreCollection<any>;
+  chats: any[] = [];
+  constructor(private afs: AngularFirestore) { }
+
+  cargarMensajes(): any {
+    this.itemsCollection = this.afs.collection<any>('chats');
+    this.itemsCollection.valueChanges().subscribe((datos: any[]) => {
+      console.log(datos);
+      return datos;
+    });
+  }
+
 }
